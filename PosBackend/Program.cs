21aa466b67754465d7ab9bbd12dc74235bd01ec7
wisesoft <atmtec@gmail.com>
2024-12-03 15:47:06 -0500

    created migrations

--- PosBackend/Program.cs
+++ PosBackend/Program.cs
@@ -1,8 +1,13 @@
+using PosBackend.models;
+using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
+
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddDbContext<ProductDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("POSDatabase")));
+
 
 var app = builder.Build();
 
@@ -21,7 +26,7 @@ var summaries = new[]
 
 app.MapGet("/weatherforecast", () =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
+    var forecast = Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
         (
             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
